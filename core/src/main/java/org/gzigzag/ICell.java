55b1f5c4ed9ee993d2c2bb58c26e05f6fb46d12d
Евгений Терехов <nergal@psu.ru>
2025-06-13 09:46:20 +0400

    refactor: new maven module for functional core

---
+++ core/src/main/java/org/gzigzag/ICell.java
@@ -0,0 +1,8 @@
+package org.gzigzag;
+
+public interface ICell {
+    /**
+     * Obtain the unique ID for this cell.
+     */
+    String getID();
+}
---
+++ core/src/main/java/org/gzigzag/ZZBaseCell.java
@@ -0,0 +1,15 @@
+package org.gzigzag;
+
+public abstract class ZZBaseCell implements ICell {
+    protected final String id;
+
+    protected ZZBaseCell(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String getID() {
+        return this.id;
+    }
+
+}
